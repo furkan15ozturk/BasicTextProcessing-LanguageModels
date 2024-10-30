eced8d8530267904766ac67f4a6f89772bb3e955
Furkan Öztürk <58707213+furkan15ozturk@users.noreply.github.com>
2024-10-30 19:39:44 +0300

    Changes bla bla

--- basicTextProcessing.py
+++ basicTextProcessing.py
@@ -1,51 +1,97 @@
-with open("hw01_bilgisayar.txt", "r", encoding="utf-8") as file:
-    text = file.read()
-
-# ************************************************************************
-
 import turkish_tokenizer
+from itertools import islice
 
-tokenizer = turkish_tokenizer.TurkishTokenizer()
 
 # ************************************************************************
 
-example_text = "Bu 1. bir örnektir. Noktadan önce sayı var. Test 3. noktaya işaret."
+def first_step(text):
+    tokenizer = turkish_tokenizer.TurkishTokenizer()
 
-processed_text = tokenizer.preprocessText(text)
+    processed_text = tokenizer.preprocess_text(text)
+    sentences = tokenizer.sentence_tokenizer(processed_text)
+    sentence_count = len(sentences)
 
-print(processed_text)
-
-# ************************************************************************
-
-"""
-words = tokenizer.tokenize(text)
-
-print("Token Count:", len(words))
-
-with open("output.txt", "w", encoding="utf-8") as output_file:
-    for word in words:
-        output_file.write(word + "\n")
-
-# ************************************************************************
-
-vocabFrequencies = tokenizer.vocabFreq(text)
-unique_words = set(vocabFrequencies)
-print("Vocab Count:", len(unique_words))
-print(vocabFrequencies)"""
-
-"""text1 = '''Seq Sentence 
-1   Let's try to be Good.
-2   Being good doesn't make sense.
-3   Good is always good.'''"""
-
-"""words = word_tokenize(text)
-fdist1 = FreqDist(words)
-
-filtered_word_freq = dict((word, freq) for word, freq in fdist1.items() if not word.isdigit())
-
-print(filtered_word_freq)"""
+    tokens = tokenizer.word_tokenizer(processed_text)
+    token_count = len(tokens)
 
 
-# with open("output_words.txt", "w", encoding="utf-8") as output_file:
-#     for word, frequencies in vocabFrequencies:
-#         output_file.write(word + ":" + frequencies + "\n")
+    vocab = tokenizer.vocab(processed_text)
+    vocab_size = len(vocab)
+
+    unigram_prob = tokenizer.ngram_probabilities(processed_text, add_k_smoothing = False, n = 1)
+
+    bigram_prob = tokenizer.ngram_probabilities(processed_text, add_k_smoothing = False, n = 2)
+
+    # print(sentences)
+
+    return sentence_count, token_count, vocab_size, unigram_prob, bigram_prob
+
+def second_step(text):
+    tokenizer = turkish_tokenizer.TurkishTokenizer()
+
+    processed_text = tokenizer.preprocess_text(text)
+
+    updated_text_unk = tokenizer.replace_unk(processed_text)
+
+    k_smoothed_bigram_probability = tokenizer.ngram_probabilities(updated_text_unk, add_k_smoothing = True, k = 0.5, n = 2)
+    sorted_k_smoothed_bigram_probability = dict(sorted(k_smoothed_bigram_probability.items(), key=lambda item: item[1][2], reverse=True))
+    top_100_sorted_k_smoothed_bigram_probability = dict(islice(sorted_k_smoothed_bigram_probability.items(), 100))
+    # print(top_100_sorted_k_smoothed_bigram_probability)
+
+    return  top_100_sorted_k_smoothed_bigram_probability
+
+def third_step(sentence, text):
+    tokenizer = turkish_tokenizer.TurkishTokenizer()
+
+    processed_text = tokenizer.preprocess_text(text)
+
+    updated_text_unk = tokenizer.replace_unk(processed_text)
+
+    vocab = tokenizer.vocab(processed_text)
+
+    k_smoothed_bigram_probability = tokenizer.ngram_probabilities(updated_text_unk, add_k_smoothing=True, k=0.5, n=2)
+    print(k_smoothed_bigram_probability)
+    probability_of_sentences = tokenizer.prob_of_a_given_corpus(sentence, k_smoothed_bigram_probability, vocab, n=2)
+    return probability_of_sentences
+
+
+if __name__ == "__main__":
+
+    with open("hw01_bilgisayar.txt", "r", encoding="utf-8") as file:
+        text = file.read()
+
+    # ************************************************************************
+
+    sentence_count, token_count, vocab_size, unigram_prob, bigram_prob = first_step(text)
+    top_100_sorted_k_smoothed_bigram_probability = second_step(text)
+
+    sentence_1 = "İlk elektrikli bilgisayar çalışmakta."
+    sentence_2 =  "Prof. Dr. Ahmet Kovalı'nın sayesinde bunu başarabildik."
+
+    prob_of_sentence_1 = third_step(sentence_1, text)
+    print(prob_of_sentence_1)
+    # file_name = "hw01_tinytr_Result.txt"
+    # with open(file_name, "w", encoding="utf-8") as f:
+    #     # Write the number of sentences
+    #     f.write(f"Number of Sentences in File: {sentence_count}\n")
+    #
+    #     # Write the total token count
+    #     f.write(f"Number of Total Tokens (Corpus Size): {token_count}\n")
+    #
+    #     # Write the vocabulary size
+    #     f.write(f"Number of Unique Words (Vocabulary Size): {vocab_size}\n\n")
+    #
+    #     # Write Unigram Probabilities
+    #     f.write("Unigrams Sorted wrt Frequencies (from Higher to Lower Frequencies):\n")
+    #     for unigram, (count, prob) in unigram_prob.items():
+    #         f.write(f"{unigram}: ( {count},   {prob} )\n")
+    #
+    #     # Write Bigram Probabilities
+    #     f.write("\nBigrams Sorted wrt Frequencies (from Higher to Lower Frequencies):\n")
+    #     for bigram, (count, prob) in bigram_prob.items():
+    #             f.write(f"{bigram}: ( {count},   {prob} )\n")
+    #
+    #     # Write Bigram Probabilities
+    #     f.write("\nTop 100 Bigrams wrt SmoothedProbability (from Higher to Lower):\n")
+    #     for bigram, (count, smoothed_prob, prob) in top_100_sorted_k_smoothed_bigram_probability.items():
+    #         f.write(f"{bigram}: ( {smoothed_prob},   {prob} )\n")
--- turkish_tokenizer.py
+++ turkish_tokenizer.py
@@ -1,40 +1,170 @@
-from audioop import avgpp
-
 import regex as re
+from collections import Counter
 
 class TurkishTokenizer:
 
-    def preprocessText(self, text):
+    @staticmethod
+    def preprocess_text(text):
         text = text.lower()
         target_characters = {'.', '!', '?'}
-        abbreviations = ['Dr', 'Prof']
+
+        #Abbreviations - This list can be expanded. This is just example.
+        medical_abbreviations = ['dr', 'prof', 'doç']
+        military_abbreviations = ['as', 'iz', 'asb', 'astsb', 'atğm', 'bçvş', 'bl', 'bnb',
+                                  'çvş', 'dz', 'kuv', 'k', 'gen', 'gnkur', 'hv', 'kuv', 'svn',
+                                  'ıs', 'ısth', 'kad', 'kd', 'kora', 'korg', 'kur', 'bşk', 'lv',
+                                  'mu', 'nö', 'sb', 'onb', 'or', 'ora', 'ord', 'org', 'p', 'tb',
+                                  'tğm', 'tnk', 'top', 'top', 'tug', 'tuğa', 'tüm', 'tüma', 'tümg',
+                                  'ulş', 'uz', 'çvş', 'uzm', 'üçvş', 'ütğm', 'yb', 'yd', 'yzb']
+
+        abbreviations = medical_abbreviations + military_abbreviations
+
+        if text[-1] in target_characters:
+            text = text[:-1] + ""
+
         for i in range (len(text)):
-            number_pattern = r"\d\."
             if text[i] in target_characters:
-                if i > 0 and re.match(r"\d", text[i-1]):
+                # checking if a number comes before the dot, also checks if the sentence ends with a number and ! or ?
+                # this solution does not work for the sentences that and with a number and a dot. For example:
+                # "Bu sorunun cevabı 7."
+                # To solve this problem we would need to know the meaning of the number and why it is used
+                # Is it seven? or seventh? The solution takes it as "seventh" and does not consider the probability of
+                # it being "seven".
+                # But for the given examples of hw01_tinytr.txt and hw01_bilgisayar.txt, it works just fine.
+                if i > 0 and re.match(r"\d", text[i-1]) and (text[i] != "!" or text[i] == "?"):
                     continue
+
                 # checking abbreviations
                 for abbr in abbreviations:
                     if text[max(0, i - len(abbr) - 1):i].strip() == abbr:
                         break
+
                 else:
-                    text = text[:i] + "<\\s>" + text[i + 1:]
+                    text = text[:i] + "<\\s> <s>" + text[i + 1:]
+
+        text = "<s> " + text.strip() + " <\\s>"
         text = re.sub(r"\s+", " ", text)
         return text
-    
-    def tokenize(self, text):
+
+    @staticmethod
+    def word_tokenizer(text):
         text = text.lower()
+        punctuations = [
+            '.', ',', '!', '?', ';', ':', '-', '_', '(', ')', '[', ']', '{', '}',
+            '"', "'", '...', '–', '—', '/', '\\', '|', '@', '#', '$', '%', '^', '&',
+            '*', '+', '=', '<', '>', '`', '~'
+        ]
+        tokens = re.findall(r"<s>|<\\s>|\w+|[^\w\s]", text, re.UNICODE)
+        cleaned_tokens = [token for token in tokens if token not in punctuations]
 
-        tokens = re.findall(r"\w+|[^\w\s]", text, re.UNICODE)
-
-        return tokens
-    
-    def vocabFreq(self, text):
-        tokens = re.findall(r"\w+", text, re.UNICODE)
+        return cleaned_tokens
+    @staticmethod
+    def vocab_freq(text):
+        tokens = re.findall(r"<s>|<\\s>|\w+", text, re.UNICODE)
         vocabulary = {}
         for word in tokens:
             if word in vocabulary:
                 vocabulary[word] += 1
-            else: 
+            else:
                 vocabulary[word] = 1
         return vocabulary
+
+    @staticmethod
+    def vocab(text):
+        tokens = re.findall(r"<s>|<\\s>|\w+", text, re.UNICODE)
+        return set(tokens)
+
+    @staticmethod
+    def sentence_tokenizer(text):
+        sentences = re.findall(r"<s>.*?<\\s>", text)
+        return sentences
+
+    @staticmethod
+    def n_gram(text, n = 1):
+        ngram_complete = []
+        sentences = TurkishTokenizer.sentence_tokenizer(text)
+        for sentence in sentences:
+            tokens = TurkishTokenizer.word_tokenizer(sentence)
+            for i in range(len(tokens) - n + 1):
+                ngram = tuple(tokens[i:i + n])
+                ngram_complete.append(ngram)
+        return ngram_complete
+
+    @staticmethod
+    def sort_ngrams(ngram):
+        ngram_frequencies = Counter(ngram)
+        sorted_ngrams = ngram_frequencies.most_common()
+        return sorted_ngrams
+
+    @staticmethod
+    def ngram_probabilities(text, add_k_smoothing = True, k = 1, n = 1):
+        ngrams = TurkishTokenizer.n_gram(text, n)
+        sorted_ngrams = TurkishTokenizer.sort_ngrams(ngrams)
+        vocab_size = len(TurkishTokenizer.vocab(text))
+        tokens = TurkishTokenizer.word_tokenizer(text)
+        total_words = len(tokens)
+        if n > 1:
+            context_counts = Counter(TurkishTokenizer.n_gram(text, n - 1))
+        else:
+            context_counts = Counter(ngrams)
+
+        # Calculate n-gram probabilities
+        ngram_probs = {}
+        for ngram, count in sorted_ngrams:
+            context = ngram[:n - 1] if n > 1 else ngram
+            if n == 1:
+                probability = count / total_words
+                k_smoothed_probability = (count * k) / (total_words + k * vocab_size)
+            else:
+                probability = count / context_counts[context]
+                k_smoothed_probability = (count * k) / (context_counts[context] + k * vocab_size)
+            if add_k_smoothing:
+                ngram_probs[ngram] = (count, k_smoothed_probability, probability)
+            else:
+                ngram_probs[ngram] = (count, probability)
+
+        ngram_probs[('unk', 'unk')] = (0, k/(k*vocab_size), 0)
+        return ngram_probs
+
+    @staticmethod
+    def replace_unk(text):
+        word_frequencies = TurkishTokenizer.vocab_freq(text)
+
+        min_token = min(word_frequencies, key=word_frequencies.get)
+        min_frequency = word_frequencies[min_token]
+
+        text = text.replace(f" {min_token} ", " UNK ")
+
+        word_frequencies['UNK'] = word_frequencies.get('UNK', 0) + min_frequency
+        del word_frequencies[min_token]
+        return text
+
+    @staticmethod
+    def prob_of_a_given_corpus(text, ngram_prob, vocabulary, n=1):
+        processed_text = TurkishTokenizer.preprocess_text(text)
+        print(processed_text)
+        ngrams = TurkishTokenizer.n_gram(processed_text, n)  # Generate bigrams (n=2)
+        print(ngrams)
+
+        total_probability = 1.0
+
+        for ngram in ngrams:
+            # Check if the n-gram exists in the ngram_prob
+            probability = 1.0
+            if ngram in ngram_prob:
+                # If found, get the probability
+                probability = ngram_prob[ngram][1]  # Second value is the probability
+                print(f"Probability of {ngram}: {probability}")
+            else:
+                updated_ngram = tuple('unk' if word not in vocabulary else word for word in ngram)
+                try:
+                    probability = ngram_prob[updated_ngram][1]  # Second value is the probability
+                except:
+                    print(f"Probability of {updated_ngram}: 0")
+                print(f"Probability of {updated_ngram}: {probability}")
+
+
+            # Update the total probability (assuming independence of events)
+            total_probability *= probability
+
+        return total_probability
